cafc887881ae7e7483d47ff03b15b154f3cd5d24
VLAD-ASUS-4090\wladdyka <wladdyka@gmail.com>
2023-07-21 22:38:03 +0600

    basic glfw glew glm project setup

--- main.cpp
+++ main.cpp
@@ -1,9 +1,113 @@
 #include <cstdio>
+#include <cstring>
+#include <cmath>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-const GLint WIDTH = 800, HEIGHT = 600;
+const GLint WIDTH = 1920, HEIGHT = 1080;
+const float toRadians = 3.14159265f / 180.0f;
+GLuint vaoId, vboId, shaderId, uniformModelId;
+
+bool directionRight = true;
+float triangleOffset = 0.0f;
+float triangleMaxOffset = 0.7f;
+float triangleOffsetIncrement = 0.0005f;
+
+static const char* vertexShaderCode = "#version 330\n"
+                                      "layout (location = 0) in vec3 pos;\n"
+                                      "uniform mat4 model;\n"
+                                      "void main() {\n"
+                                      "gl_Position = model * vec4(0.2 * pos.x, 0.2 * pos.y, pos.z, 1.0);\n"
+                                      "}";
+
+static const char* fragmentShaderCode = "#version 330\n"
+                                        "out vec4 colour;\n"
+                                        "void main() {\n"
+                                        "colour = vec4(1.0, 0.0, 0.0, 1.0);\n"
+                                        "}";
+
+void AddShader(GLuint programId, const char* programCode, GLenum shaderType, const char* shaderName) {
+    GLuint shaderToAdd = glCreateShader(shaderType);
+
+    const GLchar* shaderCode[1];
+    shaderCode[0] = programCode;
+
+    GLint codeLength[1];
+    codeLength[0] = strlen(programCode);
+
+    glShaderSource(shaderToAdd, 1, shaderCode, codeLength);
+    glCompileShader(shaderToAdd);
+
+    GLint compilationResult = 0;
+    GLchar errorLog[1024] = { 0 };
+
+    glGetShaderiv(shaderToAdd, GL_COMPILE_STATUS, &compilationResult);
+    if (!compilationResult) {
+        glGetShaderInfoLog(shaderToAdd, sizeof(errorLog), nullptr, errorLog);
+        printf("Error compiling shader %s program: %s", shaderName, errorLog);
+        return;
+    }
+
+    glAttachShader(programId, shaderToAdd);
+}
+
+void CompileShaders() {
+    shaderId = glCreateProgram();
+
+    if (!shaderId) {
+        printf("Error creating shader program");
+        return;
+    }
+
+    AddShader(shaderId, vertexShaderCode, GL_VERTEX_SHADER, "vertex");
+    AddShader(shaderId, fragmentShaderCode, GL_FRAGMENT_SHADER, "fragment");
+
+    GLint linkingResult = 0;
+    GLchar errorLog[1024] = { 0 };
+
+    glLinkProgram(shaderId);
+    glGetProgramiv(shaderId, GL_LINK_STATUS, &linkingResult);
+    if (!linkingResult) {
+        glGetProgramInfoLog(shaderId, sizeof(errorLog), nullptr, errorLog);
+        printf("Error linking shader program: %s", errorLog);
+        return;
+    }
+
+    glValidateProgram(shaderId);
+    glGetProgramiv(shaderId, GL_VALIDATE_STATUS, &linkingResult);
+    if (!linkingResult) {
+        glGetProgramInfoLog(shaderId, sizeof(errorLog), nullptr, errorLog);
+        printf("Error validating shader program: %s", errorLog);
+        return;
+    }
+
+    uniformModelId = glGetUniformLocation(shaderId, "model");
+}
+
+void CreateTriangle () {
+    GLfloat vertices[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f
+    };
+
+    glGenVertexArrays(1, &vaoId);
+    glBindVertexArray(vaoId);
+
+    glGenBuffers(1, &vboId);
+    glBindBuffer(GL_ARRAY_BUFFER, vboId);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
 
 int main() {
     if (!glfwInit()) {
@@ -38,11 +142,38 @@ int main() {
 
     glViewport(0, 0, bufferWidth, bufferHeight);
 
+    CreateTriangle();
+    CompileShaders();
+
     while(!glfwWindowShouldClose(mainWindow)) {
         glfwPollEvents();
 
-        glClearColor(0, 0.5f, 0, 1);
+        if (directionRight) {
+            triangleOffset += triangleOffsetIncrement;
+        } else {
+            triangleOffset -= triangleOffsetIncrement;
+        }
+
+        if (std::abs(triangleOffset) >= triangleMaxOffset) {
+            directionRight = !directionRight;
+        }
+
+        glClearColor(0, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderId);
+
+        glm::mat4 model(1.0f);
+        model = glm::translate(model, glm::vec3(triangleOffset, 0.0f, 0.0f));
+
+        glUniformMatrix4fv(uniformModelId, 1, GL_FALSE, glm::value_ptr(model));
+
+        glBindVertexArray(vaoId);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glBindVertexArray(0);
+        glUseProgram(0);
 
         glfwSwapBuffers(mainWindow);
     }
