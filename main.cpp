d68b18aae9e0d2e6c4d938de70fa54417af0ebb0
VLAD-ASUS-4090\wladdyka <wladdyka@gmail.com>
2023-07-23 11:46:01 +0600

    added projection matrix

--- main.cpp
+++ main.cpp
@@ -10,24 +10,29 @@
 
 const GLint WIDTH = 1920, HEIGHT = 1080;
 const float toRadians = 3.14159265f / 180.0f;
-GLuint vaoId, vboId, shaderId, uniformModelId;
+GLuint vaoId, vboId, iboId, shaderId, uniformModelId, uniformProjectionId;
 
 bool directionRight = true;
 float triangleOffset = 0.0f;
 float triangleMaxOffset = 0.7f;
 float triangleOffsetIncrement = 0.0005f;
+float currentAngle = 0.0f;
 
 static const char* vertexShaderCode = "#version 330\n"
-                                      "layout (location = 0) in vec3 pos;\n"
-                                      "uniform mat4 model;\n"
-                                      "void main() {\n"
-                                      "gl_Position = model * vec4(0.2 * pos.x, 0.2 * pos.y, pos.z, 1.0);\n"
+                                      "layout (location = 0) in vec3 pos;"
+                                      "out vec4 vertexColour;"
+                                      "uniform mat4 model;"
+                                      "uniform mat4 projection;"
+                                      "void main() {"
+                                      "gl_Position = projection * model * vec4(pos, 1.0);"
+                                      "vertexColour = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);"
                                       "}";
 
 static const char* fragmentShaderCode = "#version 330\n"
-                                        "out vec4 colour;\n"
-                                        "void main() {\n"
-                                        "colour = vec4(1.0, 0.0, 0.0, 1.0);\n"
+                                        "out vec4 colour;"
+                                        "in vec4 vertexColour;"
+                                        "void main() {"
+                                        "colour = vertexColour;"
                                         "}";
 
 void AddShader(GLuint programId, const char* programCode, GLenum shaderType, const char* shaderName) {
@@ -74,7 +79,6 @@ void CompileShaders() {
     if (!linkingResult) {
         glGetProgramInfoLog(shaderId, sizeof(errorLog), nullptr, errorLog);
         printf("Error linking shader program: %s", errorLog);
-        return;
     }
 
     glValidateProgram(shaderId);
@@ -82,21 +86,33 @@ void CompileShaders() {
     if (!linkingResult) {
         glGetProgramInfoLog(shaderId, sizeof(errorLog), nullptr, errorLog);
         printf("Error validating shader program: %s", errorLog);
-        return;
     }
 
     uniformModelId = glGetUniformLocation(shaderId, "model");
+    uniformProjectionId = glGetUniformLocation(shaderId, "projection");
 }
 
 void CreateTriangle () {
+    unsigned int indices[] = {
+           0, 3, 1,
+           1, 3, 2,
+           2, 3, 0,
+           0, 1, 2
+    };
+
     GLfloat vertices[] = {
             -1.0f, -1.0f, 0.0f,
+            0.0f, -1.0f, 1.0f,
             1.0f, -1.0f, 0.0f,
             0.0f, 1.0f, 0.0f
     };
 
     glGenVertexArrays(1, &vaoId);
     glBindVertexArray(vaoId);
+
+    glGenBuffers(1, &iboId);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glGenBuffers(1, &vboId);
     glBindBuffer(GL_ARRAY_BUFFER, vboId);
@@ -105,6 +121,7 @@ void CreateTriangle () {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
     glEnableVertexAttribArray(0);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 }
@@ -140,10 +157,13 @@ int main() {
         return 1;
     }
 
+    glEnable(GL_DEPTH_TEST);
     glViewport(0, 0, bufferWidth, bufferHeight);
 
     CreateTriangle();
     CompileShaders();
+
+    glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth / (GLfloat)bufferHeight, 0.1f, 100.0f);
 
     while(!glfwWindowShouldClose(mainWindow)) {
         glfwPollEvents();
@@ -154,25 +174,36 @@ int main() {
             triangleOffset -= triangleOffsetIncrement;
         }
 
+        currentAngle += 0.01f;
+        if (currentAngle >= 360) {
+            currentAngle -= 360;
+        }
+
         if (std::abs(triangleOffset) >= triangleMaxOffset) {
             directionRight = !directionRight;
         }
 
         glClearColor(0, 0, 0, 1);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderId);
 
         glm::mat4 model(1.0f);
-        model = glm::translate(model, glm::vec3(triangleOffset, 0.0f, 0.0f));
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -5.0f));
+        model = glm::rotate(model, currentAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+        //model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));
 
         glUniformMatrix4fv(uniformModelId, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(uniformProjectionId, 1, GL_FALSE, glm::value_ptr(projection));
 
         glBindVertexArray(vaoId);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
 
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
+
         glUseProgram(0);
 
         glfwSwapBuffers(mainWindow);
